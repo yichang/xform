0c5f13a7411c6c6d67c80fb516c874fada243168
YiChang Shih <yichangshih@31-34-235.wireless.csail.mit.edu>
2014-12-10 19:55:46 -0500

    make file

--- Plasma/jni/Android.mk
+++ Plasma/jni/Android.mk
@@ -2,8 +2,22 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := plasma
-LOCAL_SRC_FILES := plasma.c
+#LOCAL_MODULE    := plasma
+#LOCAL_SRC_FILES := plasma.c
+
+ROOT = ../../
+NDK = ../../../android-ndk-r10d/
+LOCAL_MODULE    := filter
+LOCAL_SRC_FILES := boxblur.cpp
+ARCH = armeabi-v7a
+LOCAL_C_INCLUDES += $(ROOT)
+LOCAL_C_INCLUDES += $(ROOT)/third_party/eigen-eigen-1306d75b4a21/
+LOCAL_C_INCLUDES += $(NDK)/sources/cxx-stl/gnu-libstdc++/4.9/include/
+LOCAL_C_INCLUDES += $(NDK)/sources/cxx-stl/gnu-libstdc++/4.9/libs/$(ARCH)/include/
+
+#LOCAL_MODULE    := ndk1
+#LOCAL_SRC_FILES := naive.c
+
 LOCAL_LDLIBS    := -lm -llog -ljnigraphics
 
 include $(BUILD_SHARED_LIBRARY)
