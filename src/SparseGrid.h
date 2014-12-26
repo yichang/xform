83df3253ba1299e25ff556bf0b54e566249b2f68
YiChang Shih <yichangshih@YiChangs-MacBook-Air.local>
2014-12-26 11:11:25 +0800

    Use unordered_map for sparse grid implementation

--- src/SparseGrid.h
+++ src/SparseGrid.h
@@ -1,4 +1,7 @@
 #include <Eigen/Dense>
+#include <unordered_map>
+#include <vector>
+
 #include "util.h"
 #include "XImage.h"
 
@@ -16,7 +19,8 @@ class SparseGrid{
   SparseGrid(){};
   void construct(const ImageType_1& im_in);  
   void construct(const XImage& im_in);
-  void blur(int dim); 
+  void blur(const int dim); 
+  void blur(const int dim, const int tap); 
 
   // Interpolation
   void resample(const ImageType_1& im_in, ImageType_1* im_out) const;
@@ -25,7 +29,15 @@ class SparseGrid{
   // Properties
   int dims() const; // dimension of the grid
  private:
-  // SparseMatrix
+  class Sample{ // Nested class to hide impl details
+   public:  
+     Sample(){};
+     unsigned int x, y, r, g, b; // TODO(yichang): init
+  }
+  SampleCount query(Sample& sample) const;
+  void add_sample(Sample& sample) const;
+  std::unordered_map<Sample, SampleCount> grid_; // spare grid
+  std::vector<float> cell_size
 };
 
 } // namespace xform
