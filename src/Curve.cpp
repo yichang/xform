0458265cf21108472de9180b9958ba194aa3bac4
YiChang Shih <yichangshih@YiChangs-MacBook-Air.local>
2014-12-28 20:02:36 +0800

    S shape

---
+++ src/Curve.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include "Curve.h"
+#include "util.h"
+
+using namespace xform;
+
+void Curve::sShape(const ImageType_1& im_in, const PixelType sigma, 
+    const PixelType g0, const double alpha, ImageType_1* im_out) const{
+  
+  const int height = im_in.rows();
+  const int width  = im_in.cols();
+  *im_out = ImageType_1(height, width);
+
+  for(int i = 0; i < height; i++){ 
+    for(int j = 0; j < width; j++){
+      const PixelType g = im_in(i, j); 
+      
+      if ((g < (g0 + sigma)) && (g >= g0)) 
+        (*im_out)(i, j) = g0 + sigma * pow((g - g0)/sigma, alpha);
+      else if ((g > (g0 - sigma)) && (g < g0)) 
+        (*im_out)(i, j) = g0 - sigma * pow((g0 - g)/sigma, alpha);
+      else
+        (*im_out)(i, j) = g;
+   }}
+}
---
+++ test/Curve_unittest.cc
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "XImage.h"
+#include "ColorSpace.h"
+#include "Curve.h"
+#include "util.h"
+
+TEST(CurveTest, s_shape){
+  std::string filename = "../images/yichang.png";
+  xform::XImage my_image, lab, new_lab(3), out; 
+  my_image.read(filename); 
+
+  xform::ColorSpace color_space;
+  color_space.rgb2lab(my_image, &lab);
+  
+  xform::Curve curve;
+  curve.sShape(lab.at(0), 10, 60, 0.2, &(new_lab.at(0)));
+  new_lab.at(1) = lab.at(1);
+  new_lab.at(2) = lab.at(2);
+
+  color_space.lab2rgb(new_lab, &out);
+  out.write("CurveTest_s_shape.png");
+}
