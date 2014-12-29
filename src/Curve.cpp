114a49a229ad4d8ab28c19d25533627a805dd5d3
YiChang Shih <yichangshih@YiChangs-MacBook-Air.local>
2014-12-29 16:47:32 +0800

    per-pixel curve adjustment

--- src/Curve.cpp
+++ src/Curve.cpp
@@ -14,12 +14,17 @@ void Curve::sShape(const ImageType_1& im_in, const PixelType sigma,
   for(int i = 0; i < height; i++){ 
     for(int j = 0; j < width; j++){
       const PixelType g = im_in(i, j); 
-      
-      if ((g < (g0 + sigma)) && (g >= g0)) 
-        (*im_out)(i, j) = g0 + sigma * pow((g - g0)/sigma, alpha);
-      else if ((g > (g0 - sigma)) && (g < g0)) 
-        (*im_out)(i, j) = g0 - sigma * pow((g0 - g)/sigma, alpha);
+      sShape(g, sigma, g0, alpha, &((*im_out)(i,j))); 
+    }}
+}
+
+void Curve::sShape(const PixelType& in, const PixelType sigma, 
+            const PixelType g0, const double alpha, PixelType* out) const{
+
+      if ((in < (g0 + sigma)) && (in >= g0)) 
+        *out = g0 + sigma * pow((in - g0)/sigma, alpha);
+      else if ((in > (g0 - sigma)) && (in < g0)) 
+        *out = g0 - sigma * pow((g0 - in)/sigma, alpha);
       else
-        (*im_out)(i, j) = g;
-   }}
+        *out = in;
 }
--- src/Curve.h
+++ src/Curve.h
@@ -13,7 +13,11 @@ class Curve{
   Curve(){};
   // r(i) = g0 + sign(i-g0) * sigma * (|i-g0|/sigma)^alpha0
   void sShape(const ImageType_1& im_in, const PixelType sigma, 
-            const PixelType g, const double alpha, ImageType_1* im_out) const;
+            const PixelType g0, const double alpha, ImageType_1* im_out) const;
+
+  void sShape(const PixelType& in, const PixelType sigma, 
+            const PixelType g0, const double alpha, PixelType* out) const;
+
   void expShape(const ImageType_1& im_in) const; //More parameters...
 
 
