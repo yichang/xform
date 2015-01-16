a61ab69e26e7236fc23bda2e617f9e99e045ebf2
YiChang Shih <yichangshih@YiChangs-MacBook-Air.local>
2015-01-17 01:04:36 +0800

    reconstruction pipeline. Bugs to be fixed

--- xform_halide/recon_separate.cpp
+++ xform_halide/recon_separate.cpp
@@ -51,11 +51,29 @@ Func gaussian_stack(Func f, const int j){
   us(x, y, _) = upsample_n(ds, j)(x, y, _);
   return us;
 }
+Func yuv2rgb(Func yuv){
+  Func rgb("rgb");
+  rgb(x, y, c) = yuv(x, y, c);
+  rgb(x, y, 0) = yuv(x, y, 0)                            + 1.140 * yuv(x, y, 2);
+  rgb(x, y, 1) = yuv(x, y, 0) - 0.395 * yuv(x, y, 1) - 0.581 * yuv(x, y, 2); 
+  rgb(x, y, 2) = yuv(x, y, 0) + 2.032 * yuv(x, y, 1); 
+  return rgb;
+}
+Func rgb2yuv(Func rgb_){
+  Func yuv_("yuv_");
+  yuv_(x, y, c) = rgb_(x, y, c);
+  yuv_(x, y, 0) =  cast<float>(0.299) * rgb_(x, y, 0) + 0.587 * rgb_(x, y, 1) + 0.144 * rgb_(x, y, 2); 
+  yuv_(x, y, 1) = cast<float>(-0.147) * rgb_(x, y, 0) - 0.289 * rgb_(x, y, 1) + 0.436 * rgb_(x, y, 2); 
+  yuv_(x, y, 2) =  cast<float>(0.615) * rgb_(x, y, 0) - 0.515 * rgb_(x, y, 1) - 0.100 * rgb_(x, y, 2); 
+  return yuv_;
+}
 int main(int argc, char **argv){
 
-  Var x, y, c, yi, yo, xi, xo, ni, no;
   const int J = 5;
   const int nbins = 4;
+  const int step = 16;
+
+  Var x, y, c, yi, yo, xi, xo, ni, no;
 
   ImageParam input(Float(32), 3),
              ac_lumin_raw(Float(32), 2),
@@ -71,16 +89,19 @@ int main(int argc, char **argv){
   Func clamped("clamped");
   clamped(x, y, c) = input(clamp(x, 0, input.width()-1), clamp(y, 0, input.height()-1), c);
 
+  Func my_yuv("yuv");
+  my_yuv(x, y, c) = rgb2yuv(clamped)(x, y, c);
+
   // High-pass features
   Func ds("ds");
-  ds(x,y,c) = downsample_n(clamped, J)(x,y,c);
+  ds(x,y,c) = downsample_n(my_yuv, J)(x,y,c);
 
   Func hp("hp");
-  hp(x, y, c) = clamped(x, y, c) - upsample_n(ds, J)(x, y, c);
+  hp(x, y, c) = my_yuv(x, y, c) - upsample_n(ds, J)(x, y, c);
 
   // Laplacian features 
   Func lumin("lumin");
-  lumin(x, y) = clamped(x, y, 0);
+  lumin(x, y) = my_yuv(x, y, 0);
 
   Func gaussian[J];
   for(int i = 0; i < J; i++)
@@ -94,15 +115,16 @@ int main(int argc, char **argv){
   Func lumin_hp("lumin_hp");
   lumin_hp(x, y) = hp(x, y, 0);
   Func curve_feat[nbins-1];
-  //RDom r(0, input.width(), 0, input.height());
-  RDom r(0, 5, 0, 5);
-  Expr maxi = maximum(lumin_hp(r.x, r.y));
-  Expr mini = minimum(lumin_hp(r.x, r.y));
-  Expr range = maxi - mini;
+  RDom r(0, input.width(), 0, input.height());
+  Func maxi("maxi"), mini("mini");
+  maxi() = maximum(lumin_hp(r.x, r.y));
+  mini() = minimum(lumin_hp(r.x, r.y));
+  Func range("range"); 
+  range() = maxi() - mini();
   for(int i = 0; i < nbins - 1; i++){
-    Expr thresh = 
-      static_cast<float>(i+1) * range / static_cast<float>(nbins) + mini;
-    curve_feat[i](x, y) = max(lumin_hp(x, y) - thresh, 0); 
+    Func thresh("thresh"); 
+    thresh() = static_cast<float>(i+1) * range() / static_cast<float>(nbins) + mini();
+    curve_feat[i](x, y) = max(lumin_hp(x, y) - thresh(), 0); 
   }
 
   // De-quantization
@@ -110,9 +132,9 @@ int main(int argc, char **argv){
   ac_lumin_range(no, ni) = ac_lumin_maxs(no, ni) - ac_lumin_mins(no, ni);
   Func ac_lumin("ac_lumin");
   const int n_chan_o_lumin = 1, n_chan_i_lumin = 4 + nbins - 1 + J - 1;
-  ac_lumin(x, y) = ac_lumin_raw(x, y) * 
+  ac_lumin(x, y) = ac_lumin_raw(x, y) +  
     ac_lumin_range((x*n_chan_o_lumin)/ac_lumin_raw.width(), (y*n_chan_i_lumin)/ac_lumin_raw.height()) + 
-    ac_lumin_mins((x*n_chan_o_lumin)/ac_lumin_raw.width(), (y*n_chan_i_lumin)/ac_lumin_raw.height());
+      ac_lumin_mins((x*n_chan_o_lumin)/ac_lumin_raw.width(), (y*n_chan_i_lumin)/ac_lumin_raw.height());
 
   Func ac_chrom_range("ac_chrom_range");
   ac_chrom_range(no, ni) = ac_chrom_maxs(no, ni) - ac_chrom_mins(no, ni);
@@ -123,13 +145,13 @@ int main(int argc, char **argv){
     ac_chrom_mins((x*n_chan_o_chrom)/ac_chrom_raw.width(), (y*n_chan_i_chrom)/ac_chrom_raw.height());
   
   // Reconstruct each patch (luminance channel)
-  const int step = 32;
-  Expr offset = ac_lumin_raw.height()/(n_chan_i_lumin);
+  Expr offset_y = ac_lumin_raw.height()/(n_chan_i_lumin);
+  Expr offset_x = ac_lumin_raw.width();
   
   // Reduce Laplacian coefficients
   Func reduced_laplacian[J-1]; 
   for(int i = 0; i < J - 1; i++)
-    reduced_laplacian[i](x, y) = laplacian[i](x, y) * ac_lumin(x/step, y/step + offset * (4 + i));
+    reduced_laplacian[i](x, y) = laplacian[i](x, y) * ac_lumin(x/step, y/step + offset_y * (4 + i));
 
   for(int i = 1; i < J - 1; i++)
     reduced_laplacian[i](x, y) = reduced_laplacian[i](x, y) + reduced_laplacian[i-1](x,y);
@@ -137,34 +159,48 @@ int main(int argc, char **argv){
   // Reduce Histogram Coefficients
   Func reduced_curve_feat[nbins-1]; 
   for(int i = 0; i < nbins - 1; i++)
-    reduced_curve_feat[i](x, y) = curve_feat[i](x, y) * ac_lumin(x/step, y/step + offset * (4 + J - 1 + i));
+    reduced_curve_feat[i](x, y) = curve_feat[i](x, y) * ac_lumin(x/step, y/step + offset_y * (4 + J - 1 + i));
 
   for(int i = 1; i < nbins - 1; i++)
     reduced_curve_feat[i](x, y) = reduced_curve_feat[i](x, y) + reduced_curve_feat[i-1](x,y);
 
+  // Put together
   Func lumin_out("lumin_out");
   RDom z(0, 3);
-  lumin_out(x, y) = sum(hp(x, y, z) * ac_lumin(x/step, y/step + offset * z)) + 
-                                      ac_lumin(x/step, y/step + offset * 3)  + // affine term 
-                                      reduced_laplacian[J-2](x, y) + 
-                                      reduced_curve_feat[nbins-2](x, y); 
+  lumin_out(x, y) = sum(hp(x, y, z) *  ac_lumin(x/step, y/step + offset_y * z)) + 
+                                       ac_lumin(x/step, y/step + offset_y * 3) +
+                                       reduced_laplacian[J-2](x, y) + 
+                                       reduced_curve_feat[nbins-2](x, y); 
+  // Now for chrominance channel
+  Func chrom_out("chrom_out");
+  chrom_out(x, y, c) =  sum(hp(x, y, z) * ac_chrom(x/step + offset_x * c, y/step + offset_y * z)) + 
+                                          ac_chrom(x/step + offset_x * c, y/step + offset_y * 3);  
+  // Combine Y and UV
+  Func yuv_out("yuv_out");
+  yuv_out(x, y, c) = my_yuv(x, y, c);
+  yuv_out(x, y, 0) = lumin_out(x, y);
+  yuv_out(x, y, 1) = chrom_out(x, y, 0);
+  yuv_out(x, y, 2) = chrom_out(x, y, 1);
 
-  // Contribution of high-pass features
-  // Contribution of Laplacian features
-  // Contribution of curve features
-  
+  // YUV2RGB
+  Func rgb_out("rgb_out");
+  rgb_out(x, y, c) = yuv2rgb(yuv_out)(x, y, c);
 
-  // Reconstruct each patch (chrominance channel)
-  // Contribution of high-pass features
-  
+  // Put the upsampled DC back to predicted highpass
+  Func clamped_dc("clamped_dc");
+  clamped_dc(x, y, c) = dc(clamp(x, 0, dc.width()-1), clamp(y, 0, dc.height()-1), c);
+
+  Func new_dc("new_dc");
+  new_dc(x, y, c) = upsample_n(clamped_dc, 5)(x, y, c);
 
   Func final("final");
-  //final(x, y) = 0.5 + curve_feat[2](x, y);
-  final(x, y) = lumin_out(x, y) + 0.5;
-  //final(x, y) = reduced_laplacian[0](x, y);
+  final(x, y, c) = clamp(new_dc(x, y, c)  + rgb_out(x, y, c), 0.0f, 1.0f);
 
   /* Scheduling */
   //final.tile(x, y, xo, xi, yo, yi, 16, 32).parallel(yo).vectorize(xi,8);
+  maxi.compute_root();
+  mini.compute_root();
+  range.compute_root();
   
   std::vector<Argument> args(9);
   args[0] = input;
--- xform_halide/test_recon_separate.cpp
+++ xform_halide/test_recon_separate.cpp
@@ -16,29 +16,31 @@ int main(int argc, char** argv){
   const int num_lumin_feat = 11;
   const int num_chrom_feat = 4;
   const int meta_len = 2 * (num_lumin_feat + 2 * num_chrom_feat);
-  std::ofstream out_file;                                                   
+  std::ifstream in_file;                                                   
   float* meta = new float[meta_len];
-  out_file.open(argv[5]);                                                
+  in_file.open(argv[5]);                                                
   for(int i=0; i < meta_len; i++)
-    out_file<<meta[i]<<" ";                              
-  out_file.close();                                   
+    in_file>>meta[i]; 
+  in_file.close();                                   
 
   Image<float> ac_lumin_mins(1, num_lumin_feat), 
               ac_lumin_maxs(1, num_lumin_feat);
-  int offset=0;
   for(int y= 0; y < ac_lumin_mins.height(); y++){
     for(int x = 0; x < ac_lumin_mins.width(); x++){ 
-      ac_lumin_mins(x,y) = meta[4*y + x];
-      ac_lumin_maxs(x,y) = meta[4*y + x + num_lumin_feat];
-      offset++;
+      ac_lumin_mins(x,y) = meta[y];
+      ac_lumin_maxs(x,y) = meta[y + num_lumin_feat];
+      //cout << "min "<<x<<","<<y<<"="<<ac_lumin_mins(x,y)<<endl;
+      //cout << "max "<<x<<","<<y<<"="<<ac_lumin_maxs(x,y)<<endl;
     }}
 
   Image<float> ac_chrom_mins(2, num_chrom_feat), 
                ac_chrom_maxs(2, num_chrom_feat);
   for(int y= 0; y < ac_chrom_mins.height(); y++){
     for(int x = 0; x < ac_chrom_mins.width(); x++){ 
-      ac_chrom_mins(x,y) = meta[4*y + x + offset];
-      ac_chrom_maxs(x,y) = meta[4*y + x + 2 * num_chrom_feat + offset];
+      ac_chrom_mins(x,y) = meta[2*y + x + 2 * num_lumin_feat];
+      ac_chrom_maxs(x,y) = meta[2*y + x + 2 * num_chrom_feat + 2*num_lumin_feat];
+      //cout << "min "<<x<<","<<y<<"="<<ac_chrom_mins(x,y)<<endl;
+      //cout << "max "<<x<<","<<y<<"="<<ac_chrom_maxs(x,y)<<endl;
     }}
 
   const int level = 3;
