aea57dff3c53458049c787bf03dade6a52c145f6
YiChang Shih <yichangshih@YiChangs-MacBook-Air.local>
2015-02-02 12:11:34 +0800

    revmoed blur (unused)

--- xform_halide/recon_separate.cpp
+++ xform_halide/recon_separate.cpp
@@ -1,100 +1,10 @@
 #include <Halide.h>
+#include "resize.h"
 using namespace Halide;
 
 Var x("x"), y("y"), xi("xi"), xo("xo"), yi("yi"), yo("yo"), c("c"),
     k("k"), ni("ni"), no("no");
 
-enum InterpolationType {
-    BOX, LINEAR, CUBIC, LANCZOS
-};
-
-Expr kernel_box(Expr x) {
-    Expr xx = abs(x);
-    return select(xx <= 0.5f, 1.0f, 0.0f);
-}
-
-Expr kernel_linear(Expr x) {
-    Expr xx = abs(x);
-    return select(xx < 1.0f, 1.0f - xx, 0.0f);
-}
-
-Expr kernel_cubic(Expr x) {
-    Expr xx = abs(x);
-    Expr xx2 = xx * xx;
-    Expr xx3 = xx2 * xx;
-    float a = -0.5f;
-
-    return select(xx < 1.0f, (a + 2.0f) * xx3 - (a + 3.0f) * xx2 + 1,
-                  select (xx < 2.0f, a * xx3 - 5 * a * xx2 + 8 * a * xx - 4.0f * a,
-                          0.0f));
-}
-
-Expr sinc(Expr x) {
-    return sin(float(M_PI) * x) / x;
-}
-
-Expr kernel_lanczos(Expr x) {
-    Expr value = sinc(x) * sinc(x/3);
-    value = select(x == 0.0f, 1.0f, value); // Take care of singularity at zero
-    value = select(x > 3 || x < -3, 0.0f, value); // Clamp to zero out of bounds
-    return value;
-}
-
-struct KernelInfo {
-    const char *name;
-    float size;
-    Expr (*kernel)(Expr);
-};
-
-static KernelInfo kernelInfo[] = {
-    { "box", 0.5f, kernel_box },
-    { "linear", 1.0f, kernel_linear },
-    { "cubic", 2.0f, kernel_cubic },
-    { "lanczos", 3.0f, kernel_lanczos }
-};
-InterpolationType interpolationType = LINEAR;
-
-Func resize_x(Func f, float scaleFactor){
-
-  float kernelScaling = std::min(scaleFactor, 1.0f);
-  float kernelSize = kernelInfo[interpolationType].size / kernelScaling;
-  Expr sourcex = (x + 0.5f) / scaleFactor;
-  Func kernelx("kernelx"); 
-  Expr beginx = cast<int>(sourcex - kernelSize + 0.5f);
-  RDom domx(0, static_cast<int>(2.0f * kernelSize) + 1, "domx");
-  {
-        const KernelInfo &info = kernelInfo[interpolationType];
-        Func kx; 
-        kx(x, k) = info.kernel((k + beginx - sourcex) * kernelScaling);
-        kernelx(x, k) = kx(x, k) / sum(kx(x, domx));
-  }
-  Func resized_x("resized_x");
-  
-  kernelx.compute_root();
-  
-  resized_x(x, y, c) = sum(kernelx(x, domx) * cast<float>(f(domx + beginx, y, c)));
-  return resized_x;
-}
-Func resize_y(Func f, float scaleFactor){
-
-    float kernelScaling = std::min(scaleFactor, 1.0f);
-    float kernelSize = kernelInfo[interpolationType].size / kernelScaling;
-    Expr sourcey = (y + 0.5f) / scaleFactor;
-    Func kernely("kernely");
-    Expr beginy = cast<int>(sourcey - kernelSize + 0.5f);
-    RDom domy(0, static_cast<int>(2.0f * kernelSize) + 1, "domy");
-    {
-        const KernelInfo &info = kernelInfo[interpolationType];
-        Func ky;
-        ky(y, k) = info.kernel((k + beginy - sourcey) * kernelScaling);
-        kernely(y, k) = ky(y, k) / sum(ky(y, domy));
-    }
-    Func resized_y("resized_y");
-    resized_y(x, y, c) = sum(kernely(y, domy) * f(x, domy + beginy, c));
-
-    kernely.compute_root();
-    return resized_y;
-}
 // Downsample with a 1 3 3 1 filter
 Func downsample(Func f) {
     Func downx, downy;
@@ -164,24 +74,13 @@ Func rgb2yuv(Func rgb_){
 
   return yuv_;
 }
-Func box_blur(Func f, const int b_width){
-  
-  Func offset_x("offset_x");
-  RDom u(-b_width, b_width + 1);
-  offset_x(y,c) = sum(f(u, y, c));
-
-  Func blur_x ("blur_x");
-  blur_x(x, y, c) = (-1 * f(x - b_width, y, c) + f(x + b_width, y, c) + offset_x(y, c))/(2*b_width+1);
-
-  return blur_x;
-}
 int main(int argc, char **argv){
 
   const int J = 5;
   const int nbins = 4;
   const int step = 16;
   const float scaleFactor = float(std::pow(2, J-1));
-  bool stack = false;
+  bool stack = true;
   
 
   ImageParam input(Float(32), 3),
